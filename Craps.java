9bb7f3f0a2b04ba058c6e9fe838c3ba8fc8add19
Hudson Stimmler <Hstimmler23@students.polytechnic.org>
2022-01-18 13:37:30 -0800

    Working Die Class

--- Craps.java
+++ Craps.java
@@ -6,41 +6,8 @@
  */
 
 public class NewClass {
+    public static void main(String[] args){
     //fields
-    private double xcoord; 
-    private double ycoord;
-    private double radius; 
-
-
-    //constuctor(s)
-    public Circle(double myXcoord, double myYcoord, double myRadius){
-        xcoord = myXcoord;
-        ycoord = myYcoord;
-        radius = myRadius;
-
-    }
-
-    //methods
-    public boolean isOnCircle(double x, double y){
-        System.out.println("The points (" + x + ", " + y + ") are on the circle");
-        if (((xcoord - x) * (xcoord - x)) + ((ycoord - y) * (ycoord - y)) ==  (radius) * (radius)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
-    public double getRadius(){
-        return radius;
-    }
-
-    public void changeCenter(double a, double b){
-        xcoord = a;
-        ycoord = b;
-    }
-
-    public String getCenter(){
-        return ("" + xcoord + ", " + ycoord);
+    
     }
 }
