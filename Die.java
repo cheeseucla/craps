3b00aba123f5d64781026b3db478dd1097e4abc5
Hudson Stimmler <Hstimmler23@students.polytechnic.org>
2022-01-18 13:40:40 -0800

    Fixed Working Die Class

--- Die.java
+++ Die.java
@@ -1,10 +1,25 @@
 /**
  * 
- * @version 8-26-2021
+ * @version 1-18-2022
  * @author Hudson Stimmler
  */
 
 import java.util.*;
 public class Die{
-    public static void main(String[] args){
-       
+    //Fields
+    private int DieNum; 
+
+    //constuctor(s)
+    public DieNum(){
+        DieNum = (int)(Math.random() * 6 + 1);
+    }
+
+    //methods
+    public int getDie(){
+        return DieNum;
+    }
+}
+
+    
+
+    
