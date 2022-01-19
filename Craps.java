2600fb7283c1d24a08edf26cadcb9c6cc14a18b7
Hudson Stimmler <Hstimmler23@students.polytechnic.org>
2022-01-18 17:29:50 -0800

    progress

--- Craps.java
+++ Craps.java
@@ -4,12 +4,13 @@
  * @version 1-18-2022
  * @author Hudson Stimmler
  */
-
+import java.util.*;
 public class Craps {
     public static void main(String[] args){
+        
         Die a = new Die();
         Die b = new Die();
         int total = a.getDie() + b.getDie();
-        System.out.println("The first die is: " + a.getDie() + " The second die is: " + b.getDie() + " the total is: " + total); 
+        
     }
 }
