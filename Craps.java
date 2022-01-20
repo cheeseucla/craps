858cf493792bbfe8f012a0dae4194e11d36e7075
Hudson Stimmler <Hstimmler23@students.polytechnic.org>
2022-01-19 19:16:19 -0800

    Final Craps Game

--- Craps.java
+++ Craps.java
@@ -31,7 +31,7 @@ public class Craps {
         System.out.println("Would you like to play craps? (yes/no): ");
         Scanner input = new Scanner(System.in);
         String answer = input.next();
-        if(answer.equals("yes")){
+        if(answer.toUpperCase().equals("YES")){
             response = true;
         }
         else{
@@ -44,7 +44,7 @@ public class Craps {
         System.out.println("Do you know how to play craps? (yes/no): ");
         Scanner input = new Scanner(System.in);
         String answer = input.next();
-        if(answer.equals("no")){
+        if(answer.toUpperCase().equals("NO")){
             System.out.println("1. A player rolls two six-sided dice and adds the numbers rolled together.");
             System.out.println("2. On this first roll, a 7 or an 11 automatically wins, and a 2, 3, or 12 automatically loses, and play is over. If a 4, 5, 6, 8, 9, or 10 are rolled on this first roll, that number becomes the point.");
             System.out.println("3. The player continues to roll the two dice again until one of two things happens: either they roll the point from that first roll again, in which case they win; or they roll a 7, in which case they lose.");
@@ -110,7 +110,7 @@ public class Craps {
         Scanner bu = new Scanner(System.in);
         String again = bu.next();
         int ret = 0;
-        if(again.equals("yes")){
+        if(again.toUpperCase().equals("YES")){
             ret = 1;
         }
         else{
